fc98dc2e2e9aaa32170a3138c4a3c1dc7c4ea37d
Aleksei Slivka <alexprospero25@gmail.com>
2020-09-27 20:09:02 +0300

    Ветка для ДЗ к 1 уроку

--- mvp_lesson1/MainActivity.kt
+++ mvp_lesson1/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() , MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//for git
         val listener = View.OnClickListener {
             var mainId = 0
             when (it.id) {
